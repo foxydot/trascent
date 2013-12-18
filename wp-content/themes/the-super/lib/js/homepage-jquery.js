043c2f31c4918951c230b990c97751c553907a64
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-12-18 17:18:31 -0500

    adding icons to homepage widgets

--- wp-content/themes/the-super/lib/js/homepage-jquery.js
+++ wp-content/themes/the-super/lib/js/homepage-jquery.js
@@ -1,4 +1,7 @@
 jQuery(document).ready(function($) {
     $('.conferences .widget-title').prepend('<span class="fa-stack"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-comments fa-stack-1x fa-inverse"></i></span>');
+    $('.white-papers .widget-icon i.fa').addClass('fa-file-text-o');
+    $('.case-studies .widget-icon i.fa').addClass('fa-folder-o');
+    $('.featured-article .widget-icon i.fa').addClass('fa-bookmark-o');
 });
 
