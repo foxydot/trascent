4f7cd489673b27f5439e0947560f512f18f21860
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-05-22 15:01:50 -0400

    Featured images on pages - don’t want these to be linked anywhere. See this page as example: http://trascent.com/conference/north-america/

--- wp-content/themes/the-super/lib/inc/media.php
+++ wp-content/themes/the-super/lib/inc/media.php
@@ -30,7 +30,7 @@ function msd_post_image() {
 
     // This is the most important part!  Checks to see if the post has a Post Thumbnail assigned to it. You can delete the if conditional if you want and assume that there will always be a thumbnail
     if ( has_post_thumbnail() && is_page() ) {
-        printf( '<a title="%s" href="%s">%s</a>', get_permalink(), the_title_attribute( 'echo=0' ), genesis_get_image( array( 'size' => $size, 'attr' => $default_attr ) ) );
+        printf( '%s',  genesis_get_image( array( 'size' => $size, 'attr' => $default_attr ) ) );
     }
 
 }
